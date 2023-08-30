02ddbda134d0da733dff2489d66a98054a8237a3
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-08-30 20:34:06 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var  typed = new Typed(".text", {
+    strings:["Frontend Developer", "Backend Developer", "App Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDeelay:1000,
+    loop:true
+})
